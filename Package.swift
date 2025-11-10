37fa021cf577b7c2611d44eb762b86c48e5b3543
Kafka Liu <kafka.liu@gmail.com>
2025-11-10 21:09:58 +0800

    Remove useless

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@
 
 import PackageDescription
 
-let version = "1.0.16"
 let checksum = "51152db1bc9df2f77f8a98f8bf2295348b491ba5851f7211c5b6fe1f208b9fb8"
 let url = "https://github.com/kafkaliu/dianyaai-asr-ios-sdk/releases/download/1.0.16/DianyaaiASR.xcframework.zip"
 
